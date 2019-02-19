a3a6e3583d16f31bb6b8f7523f58c8fc8228855a
Divyansh <divi09112@gmail.com>
2019-02-20 00:17:52 +0530

    yeehaa

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express');
-
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-app.listen(3001);
+http.listen(3001);
 app.get('/abc', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.json({ data: 'Hello' });
+});
+
+io.on('connection', (socket) => {
+  socket.on('answer', (payload) => {
+    console.log(payload.answer);
+    // if(payload.answer == '1') {
+    //   socket.emit('reply', {
+	  //     reply: 'correct',
+    //   });
+    // }
+    // else {
+    //   socket.emit('reply', {
+    //     reply: 'wrong',
+    //   });
+    // }
+  });
 });
